10d1ae2a57eee15380ff639842901727236ffbdc
G5-Debugger@Mohsin <mohsin.debug@gmail.com>
2026-02-04 17:58:47 +0500

    improved User Schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -41,7 +41,16 @@ const userSchema = new Schema(
     { timestamps: true }
 );
 
-const User = mongoose.model('user', userSchema);
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
 
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
-export default User;
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
+
